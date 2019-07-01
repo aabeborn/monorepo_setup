d5da3a725efcbc4ea83f9b0d346d080fa9f6b3e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-01 13:56:52 +0000

    Deploy benn983/monorepo_setup to github.com/benn983/monorepo_setup.git:gh-pages

---
+++ precache-manifest.384168a9d964ba501eda30050e5ab2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116be770c4c9444c83da88aa52d15fbf",
+    "url": "/monorepo_setup/index.html"
+  },
+  {
+    "revision": "4dfe56b6e5213882abcb",
+    "url": "/monorepo_setup/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ba5981dcebb3b219b4fe",
+    "url": "/monorepo_setup/static/js/2.a935a316.chunk.js"
+  },
+  {
+    "revision": "4dfe56b6e5213882abcb",
+    "url": "/monorepo_setup/static/js/main.ab188535.chunk.js"
+  },
+  {
+    "revision": "5ef64eeb6c8b9d5e638d",
+    "url": "/monorepo_setup/static/js/runtime~main.3d63a790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monorepo_setup/static/media/logo.5d5d9eef.svg"
+  }
+]);
